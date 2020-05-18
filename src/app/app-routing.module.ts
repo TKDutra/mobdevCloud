d7f1fd50f0d20849c3ae499496a006e6daeadbc0
TKDutra <tatiana.katia+tati@gmail.com>
2020-05-18 22:58:34 +0000

    heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 const routes: Routes = [
  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => 
     m.TabsPageModule)},
+
   
 ];
 
